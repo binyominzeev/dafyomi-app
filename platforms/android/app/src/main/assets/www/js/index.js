a40ee40ac1256402949ede0f51d60d1556d35b36
bz <szvbinjomin@gmail.com>
2019-05-08 22:55:50 +0200

    main menu is responsive now

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -430,5 +430,15 @@ var swiper=new Swiper('.swiper-container', {
 	},
 });
 
+document.addEventListener('openMenu', function(e){
+	$("#menuDiscover").toggle();
+  //It does something when the menu is opened
+  //You can access name of menu in: e.detail.menu
+})
 
+document.addEventListener('closeMenu', function(e){
+	$("#menuDiscover").toggle();
+  //It does something when the menu is closed.
+  //You can access name of menu in: e.detail.menu
+})
 
