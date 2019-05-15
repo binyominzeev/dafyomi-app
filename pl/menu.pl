0ecbd41b9353d00c6cbea323f010e512d2dd733d
bz <szvbinjomin@gmail.com>
2019-05-15 23:52:44 +0200

    responsive menu for every masechet

--- pl/menu.pl
+++ pl/menu.pl
@@ -2,259 +2,84 @@
 use strict;
 use warnings;
 
-# ======== constants ========
-
-my $page_per_line=7;
-
 # ======== config ========
 
-#my $msch="shab";
-#my $masechet="shabbos";
-#my $masechet_title="Shabbos";
+my @masechet=qw/azarah bbasra bechoros beitzah berachos bkama bmetzia chagigah chulin erchin eruvin gitin horayos kerisus kesuvos kidushin kinim makos megilah meilah menachos midos mkatan nazir nedarim nidah pesachim rhashanah sanhedrin shabbos shekalim shevuos sotah sukah taanis tamid temurah yevamos yoma zevachim/;
 
-#my $msch="eru";
-#my $masechet="eruvin";
-#my $masechet_title="Eruvin";
+my @msch=qw/az bb bech btz ber bk bm chag chul erch eru git hor ker kes kid kin mak meg mei men mid mk naz ned nid pes rh san shab shkl shev sot suk tan tmd tem yev yom zev/;
 
-#my $msch="rh";
-#my $masechet="rhashanah";
-#my $masechet_title="Rosh Hashanah";
+my @masechet_title=qw/Avodah_Zarah Bava_Basra Bechoros Beitzah Berachos Bava_Kama Bava_Metzia Chagigah Chulin Erchin Eruvin Gitin Horayos Kerisus Kesuvos Kidushin Kinim Makos Megilah Me'ilah Menachos Midos Moed_Katan Nazir Nedarim Nidah Pesachim Rosh_Hashanah Sanhedrin Shabbos Shekalim Shevuos Sotah Sukah Ta'anis Tamid Temurah Yevamos Yoma Zevachim/;
 
-#my $msch="tan";
-#my $masechet="taanis";
-#my $masechet_title="Ta'anis";
-
-#my $msch="meg";
-#my $masechet="megilah";
-#my $masechet_title="Megilah";
-
-#my $msch="mk";
-#my $masechet="mkatan";
-#my $masechet_title="Moed Katan";
-
-#my $msch="chag";
-#my $masechet="chagigah";
-#my $masechet_title="Chagigah";
-
-#my $msch="yev";
-#my $masechet="yevamos";
-#my $masechet_title="Yevamos";
-
-#my $msch="kes";
-#my $masechet="kesuvos";
-#my $masechet_title="Kesuvos";
-
-#my $msch="ned";
-#my $masechet="nedarim";
-#my $masechet_title="Nedarim";
-
-#my $msch="naz";
-#my $masechet="nazir";
-#my $masechet_title="Nazir";
-
-#my $msch="sot";
-#my $masechet="sotah";
-#my $masechet_title="Sotah";
-
-#my $msch="git";
-#my $masechet="gitin";
-#my $masechet_title="Gitin";
-
-#my $msch="kid";
-#my $masechet="kidushin";
-#my $masechet_title="Kidushin";
-
-#my $msch="bk";
-#my $masechet="bkama";
-#my $masechet_title="Bava Kama";
-
-#my $msch="bm";
-#my $masechet="bmetzia";
-#my $masechet_title="Bava Metzia";
-
-#my $msch="bb";
-#my $masechet="bbasra";
-#my $masechet_title="Bava Basra";
-
-#my $msch="san";
-#my $masechet="sanhedrin";
-#my $masechet_title="Sanhedrin";
-
-#my $msch="mak";
-#my $masechet="makos";
-#my $masechet_title="Makos";
-
-#my $msch="shev";
-#my $masechet="shevuos";
-#my $masechet_title="Shevuos";
-
-#my $msch="az";
-#my $masechet="azarah";
-#my $masechet_title="Avodah Zarah";
-
-#my $msch="hor";
-#my $masechet="horayos";
-#my $masechet_title="Horayos";
-
-
-
-#my $msch="bech";
-#my $masechet="bechoros";
-#my $masechet_title="Bechoros";
-
-my $msch="erch";
-my $masechet="erchin";
-my $masechet_title="Erchin";
-
-my $msch="tem";
-my $masechet="temurah";
-my $masechet_title="Temurah";
-
-my $msch="ker";
-my $masechet="kerisus";
-my $masechet_title="Kerisus";
-
-my $msch="mei";
-my $masechet="meilah";
-my $masechet_title="Me'ilah";
-
-#my $msch="kin";
-#my $masechet="kinim";
-#my $masechet_title="Kinim";
-
-#my $msch="tmd";
-#my $masechet="tamid";
-#my $masechet_title="Tamid";
-
-#my $msch="mid";
-#my $masechet="midos";
-#my $masechet_title="Midos";
-
-#my $msch="nid";
-#my $masechet="nidah";
-#my $masechet_title="Nidah";
-
-#my $msch="zev";
-#my $masechet="zevachim";
-#my $masechet_title="Zevachim";
-
-#my $msch="men";
-#my $masechet="menachos";
-#my $masechet_title="Menachos";
-
-#my $msch="chul";
-#my $masechet="chulin";
-#my $masechet_title="Chulin";
-
-#Peszáchim, Sekálim, Jomá, Szuká, Bécá
-
-#my $msch="pes";
-#my $masechet="pesachim";
-#my $masechet_title="Pesachim";
-
-#my $msch="shkl";
-#my $masechet="shekalim";
-#my $masechet_title="Shekalim";
-
-#my $msch="yom";
-#my $masechet="yoma";
-#my $masechet_title="Yoma";
-
-#my $msch="suk";
-#my $masechet="sukah";
-#my $masechet_title="Sukah";
-
-#my $msch="btz";
-#my $masechet="beitzah";
-#my $masechet_title="Beitzah";
-
-
+#my $msch="ber";
+#my $masechet="berachos";
+#my $masechet_title="Berachos";
 
 # ======== initialize ========
 
-my @ls=split/\n/, `ls ../www/masechet/$masechet`;
-my $last_page=(scalar @ls)+1;
+my $msch="";
 
-my $prefix=substr($ls[0],0,2);
+open SCR, ">script.js";
 
-print "function $msch"."_page_link (num) {\n".
-	"\tmasechet=\"$masechet/$prefix\";\n".
-	"\tmasechet_show=\"$masechet_title\";\n".
-	"\tmasechet_size=$last_page;\n\n".
-	"\tpage_link(num);\n".
-	"}\n\n";
-
-my $lines=int(($last_page-1)/$page_per_line);
-my $generated=$lines*$page_per_line;
-my $remains=$last_page-1-$generated;
-
-# ======== main lines ========
-
-open OUT, ">../www/$masechet.html";
-
-print OUT "<div class=\"page\">\n".
-	"<div class=\"header blue\">\n".
-	"<div class=\"left\">\n".
-	"<button class=\"icon ion-ios-arrow-back\" onclick=\"backPage()\"></button>\n".
-	"</div>\n".
-	"<h1 class=\"title align-center\">$masechet_title</h1>\n".
-	"</div>\n\n".
-	"<div class=\"content has-header\">\n\n<br />";
-
-my $page=2;
-
-for my $line (1..$lines) {
-	#print OUT "<p>\n".
-	print OUT "<div class=\"buttons-group row pages-menu\">\n";
-	#	"<div class=\"buttons-group full\">\n";
+for my $i (0..$#masechet) {
+	my $masechet=$masechet[$i];
+	$msch=$msch[$i];
+	my $masechet_title=$masechet_title[$i];
 	
-	for (1..3) {
+	$masechet_title=~s/_/ /;
+
+	my @ls=split/\n/, `ls ../www/masechet/$masechet`;
+	my $last_page=(scalar @ls)+1;
+
+	my $prefix=substr($ls[0],0,2);
+
+	print SCR "function $msch"."_page_link (num) {\n".
+		"\tmasechet=\"$masechet/$prefix\";\n".
+		"\tmasechet_show=\"$masechet_title\";\n".
+		"\tmasechet_size=$last_page;\n\n".
+		"\tpage_link(num);\n".
+		"}\n\n";
+
+	$last_page--;
+	my $pairs=int($last_page/2);
+	my $remains=$last_page-$pairs*2;
+
+	# ======== main lines ========
+
+	open OUT, ">../www/$masechet.html";
+
+	print OUT "<div class=\"page\">\n".
+		"<div class=\"header blue\">\n".
+		"<div class=\"left\">\n".
+		"<button class=\"icon ion-ios-arrow-back\" onclick=\"backPage()\"></button>\n".
+		"</div>\n".
+		"<h1 class=\"title align-center\">$masechet_title</h1>\n".
+		"</div>\n\n".
+		"<div class=\"content has-header\">\n\n<br />";
+
+	my $page=2;
+
+	print OUT "<div class=\"buttons-group pages-menu\">\n";
+
+	for (1..$pairs) {
 		print OUT menu_button("green", $page++).
 			menu_button("green-200", $page++);
 	}
 
-	print OUT menu_button("green", $page++).
-		"</div>\n<br />\n";
-		#"</div>\n</p>\n";
+	for (1..$remains) {
+		print OUT menu_button("green", $page++);
+	}
+
+	print OUT "</div>\n\n".
+		"</div>\n</div>";
+	close OUT;
 }
 
-# ======== last line ========
-
-print OUT "<p>\n".
-	"<div class=\"buttons-group row pages-menu\">\n";
-#	"<div class=\"buttons-group full\">\n";
-
-my $pairs=int($remains/2);
-my $last=$remains % 2;
-my $white=$page_per_line;
-
-for (1..$pairs) {
-	print OUT menu_button("green", $page++).
-		menu_button("green-200", $page++);
-	
-	$white-=2;
-}
-
-if ($last == 1) {
-	print OUT menu_button("green", $page++);
-	$white--;
-}
-
-for (1..$white) {
-#	print OUT "<button class=\"white\"></button>\n";
-	print OUT "<div class=\"col\"><button class=\"white border-white radius\"></button></div>\n";
-}
-
-#print OUT "</div>\n</p>\n".
-print OUT "</div>\n\n".
-	"</div>\n</div>";
-close OUT;
+close SCR;
 
 # ======== functions ========
 
 sub menu_button {
 	my ($class, $page)=@_;
-	#return "<button class=\"$class\" onclick=\"$msch"."_page_link($page)\">$page</button>\n";
-	return "<div class=\"col\"><button class=\"$class border-teal radius\" onclick=\"$msch"."_page_link($page)\">$page</button></div>\n";
+	return "<div class=\"page-but\"><button class=\"$class border-teal radius\" onclick=\"$msch"."_page_link($page)\">$page</button></div>\n";
 }
 
---
+++ pl/script.js
@@ -0,0 +1,320 @@
+function az_page_link (num) {
+	masechet="azarah/az";
+	masechet_show="Avodah Zarah";
+	masechet_size=76;
+
+	page_link(num);
+}
+
+function bb_page_link (num) {
+	masechet="bbasra/bb";
+	masechet_show="Bava Basra";
+	masechet_size=176;
+
+	page_link(num);
+}
+
+function bech_page_link (num) {
+	masechet="bechoros/be";
+	masechet_show="Bechoros";
+	masechet_size=61;
+
+	page_link(num);
+}
+
+function btz_page_link (num) {
+	masechet="beitzah/bt";
+	masechet_show="Beitzah";
+	masechet_size=40;
+
+	page_link(num);
+}
+
+function ber_page_link (num) {
+	masechet="berachos/br";
+	masechet_show="Berachos";
+	masechet_size=64;
+
+	page_link(num);
+}
+
+function bk_page_link (num) {
+	masechet="bkama/bk";
+	masechet_show="Bava Kama";
+	masechet_size=119;
+
+	page_link(num);
+}
+
+function bm_page_link (num) {
+	masechet="bmetzia/bm";
+	masechet_show="Bava Metzia";
+	masechet_size=119;
+
+	page_link(num);
+}
+
+function chag_page_link (num) {
+	masechet="chagigah/cg";
+	masechet_show="Chagigah";
+	masechet_size=27;
+
+	page_link(num);
+}
+
+function chul_page_link (num) {
+	masechet="chulin/ch";
+	masechet_show="Chulin";
+	masechet_size=142;
+
+	page_link(num);
+}
+
+function erch_page_link (num) {
+	masechet="erchin/er";
+	masechet_show="Erchin";
+	masechet_size=34;
+
+	page_link(num);
+}
+
+function eru_page_link (num) {
+	masechet="eruvin/ev";
+	masechet_show="Eruvin";
+	masechet_size=105;
+
+	page_link(num);
+}
+
+function git_page_link (num) {
+	masechet="gitin/gi";
+	masechet_show="Gitin";
+	masechet_size=90;
+
+	page_link(num);
+}
+
+function hor_page_link (num) {
+	masechet="horayos/ho";
+	masechet_show="Horayos";
+	masechet_size=14;
+
+	page_link(num);
+}
+
+function ker_page_link (num) {
+	masechet="kerisus/kr";
+	masechet_show="Kerisus";
+	masechet_size=28;
+
+	page_link(num);
+}
+
+function kes_page_link (num) {
+	masechet="kesuvos/ks";
+	masechet_show="Kesuvos";
+	masechet_size=112;
+
+	page_link(num);
+}
+
+function kid_page_link (num) {
+	masechet="kidushin/kd";
+	masechet_show="Kidushin";
+	masechet_size=82;
+
+	page_link(num);
+}
+
+function kin_page_link (num) {
+	masechet="kinim/ml";
+	masechet_show="Kinim";
+	masechet_size=5;
+
+	page_link(num);
+}
+
+function mak_page_link (num) {
+	masechet="makos/ma";
+	masechet_show="Makos";
+	masechet_size=24;
+
+	page_link(num);
+}
+
+function meg_page_link (num) {
+	masechet="megilah/mg";
+	masechet_show="Megilah";
+	masechet_size=32;
+
+	page_link(num);
+}
+
+function mei_page_link (num) {
+	masechet="meilah/ml";
+	masechet_show="Me'ilah";
+	masechet_size=22;
+
+	page_link(num);
+}
+
+function men_page_link (num) {
+	masechet="menachos/mn";
+	masechet_show="Menachos";
+	masechet_size=110;
+
+	page_link(num);
+}
+
+function mid_page_link (num) {
+	masechet="midos/ml";
+	masechet_show="Midos";
+	masechet_size=5;
+
+	page_link(num);
+}
+
+function mk_page_link (num) {
+	masechet="mkatan/mo";
+	masechet_show="Moed Katan";
+	masechet_size=29;
+
+	page_link(num);
+}
+
+function naz_page_link (num) {
+	masechet="nazir/nz";
+	masechet_show="Nazir";
+	masechet_size=66;
+
+	page_link(num);
+}
+
+function ned_page_link (num) {
+	masechet="nedarim/nd";
+	masechet_show="Nedarim";
+	masechet_size=91;
+
+	page_link(num);
+}
+
+function nid_page_link (num) {
+	masechet="nidah/ni";
+	masechet_show="Nidah";
+	masechet_size=73;
+
+	page_link(num);
+}
+
+function pes_page_link (num) {
+	masechet="pesachim/ps";
+	masechet_show="Pesachim";
+	masechet_size=121;
+
+	page_link(num);
+}
+
+function rh_page_link (num) {
+	masechet="rhashanah/rh";
+	masechet_show="Rosh Hashanah";
+	masechet_size=35;
+
+	page_link(num);
+}
+
+function san_page_link (num) {
+	masechet="sanhedrin/sn";
+	masechet_show="Sanhedrin";
+	masechet_size=113;
+
+	page_link(num);
+}
+
+function shab_page_link (num) {
+	masechet="shabbos/sh";
+	masechet_show="Shabbos";
+	masechet_size=157;
+
+	page_link(num);
+}
+
+function shkl_page_link (num) {
+	masechet="shekalim/sk";
+	masechet_show="Shekalim";
+	masechet_size=22;
+
+	page_link(num);
+}
+
+function shev_page_link (num) {
+	masechet="shevuos/sv";
+	masechet_show="Shevuos";
+	masechet_size=49;
+
+	page_link(num);
+}
+
+function sot_page_link (num) {
+	masechet="sotah/so";
+	masechet_show="Sotah";
+	masechet_size=49;
+
+	page_link(num);
+}
+
+function suk_page_link (num) {
+	masechet="sukah/su";
+	masechet_show="Sukah";
+	masechet_size=56;
+
+	page_link(num);
+}
+
+function tan_page_link (num) {
+	masechet="taanis/tn";
+	masechet_show="Ta'anis";
+	masechet_size=31;
+
+	page_link(num);
+}
+
+function tmd_page_link (num) {
+	masechet="tamid/ml";
+	masechet_show="Tamid";
+	masechet_size=10;
+
+	page_link(num);
+}
+
+function tem_page_link (num) {
+	masechet="temurah/tm";
+	masechet_show="Temurah";
+	masechet_size=34;
+
+	page_link(num);
+}
+
+function yev_page_link (num) {
+	masechet="yevamos/ye";
+	masechet_show="Yevamos";
+	masechet_size=122;
+
+	page_link(num);
+}
+
+function yom_page_link (num) {
+	masechet="yoma/yo";
+	masechet_show="Yoma";
+	masechet_size=88;
+
+	page_link(num);
+}
+
+function zev_page_link (num) {
+	masechet="zevachim/zv";
+	masechet_show="Zevachim";
+	masechet_size=120;
+
+	page_link(num);
+}
+
